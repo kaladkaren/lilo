701861cacc155954e537a274778b9c7e5e81ad84
unknown <dianeocampo97@gmail.com>
2020-07-23 08:01:30 +0400

    Add deleted_at in tables

--- sql/dbliloapp.sql
+++ sql/dbliloapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2020 at 12:44 PM
+-- Generation Time: Jul 23, 2020 at 06:00 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -36,17 +36,18 @@ CREATE TABLE `admin` (
   `super_admin` int(1) NOT NULL DEFAULT 0,
   `is_deleted` int(1) NOT NULL DEFAULT 0,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `name`, `email`, `password`, `super_admin`, `is_deleted`, `created_at`, `updated_at`) VALUES
-(1, 'Elline Ocampo', 'edocampo@myoptimind.com', '$2y$10$XqEnkMdX015LsSekTnhxOu9DLSyHVc0n50Cp.V7synSJddfmzJhYe', 1, 0, '2020-06-29 16:42:00', '2020-07-21 09:23:11'),
-(2, 'Steven Moses', 'foxuja@mailinator.com', '$2y$10$5Maap7GS3ABucg9w5uuoceNESUamL/7yP6C4QhwZczcr0./5aIeSW', 0, 1, '2020-07-20 13:11:42', '2020-07-20 13:12:11'),
-(3, 'Shaeleigh Pugh', 'todirihyca@mailinator.com', '$2y$10$zSE8pG/uPrBGmsatTbGOS.DBP6ecQusZaXnJboit6S3idBbtDzTv6', 0, 0, '2020-07-20 13:12:28', '2020-07-20 13:33:10');
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `super_admin`, `is_deleted`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Elline Ocampo', 'edocampo@myoptimind.com', '$2y$10$XqEnkMdX015LsSekTnhxOu9DLSyHVc0n50Cp.V7synSJddfmzJhYe', 1, 0, '2020-06-29 16:42:00', '2020-07-21 09:23:11', '0000-00-00 00:00:00'),
+(2, 'Steven Moses', 'foxuja@mailinator.com', '$2y$10$5Maap7GS3ABucg9w5uuoceNESUamL/7yP6C4QhwZczcr0./5aIeSW', 0, 1, '2020-07-20 13:11:42', '2020-07-20 13:12:11', '0000-00-00 00:00:00'),
+(3, 'Shaeleigh Pugh', 'todirihyca@mailinator.com', '$2y$10$zSE8pG/uPrBGmsatTbGOS.DBP6ecQusZaXnJboit6S3idBbtDzTv6', 0, 0, '2020-07-20 13:12:28', '2020-07-20 13:33:10', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -59,16 +60,17 @@ CREATE TABLE `agency` (
   `name` varchar(300) NOT NULL,
   `is_active` int(1) NOT NULL DEFAULT 1,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `agency`
 --
 
-INSERT INTO `agency` (`id`, `name`, `is_active`, `created_at`, `updated_at`) VALUES
-(1, 'DTI', 1, '2020-07-20 16:26:11', '0000-00-00 00:00:00'),
-(2, 'DOST', 1, '2020-07-20 16:26:19', '0000-00-00 00:00:00');
+INSERT INTO `agency` (`id`, `name`, `is_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'DTI', 1, '2020-07-20 16:26:11', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'DOST', 1, '2020-07-20 16:26:19', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -83,21 +85,23 @@ CREATE TABLE `cesbie_visitors` (
   `place_of_origin` varchar(300) NOT NULL,
   `pin_code` varchar(300) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cesbie_visitors`
 --
 
-INSERT INTO `cesbie_visitors` (`id`, `staff_id`, `temperature`, `place_of_origin`, `pin_code`, `created_at`, `updated_at`) VALUES
-(1, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:50:23', '0000-00-00 00:00:00'),
-(2, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:52:22', '0000-00-00 00:00:00'),
-(3, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:57:43', '0000-00-00 00:00:00'),
-(4, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:58:01', '0000-00-00 00:00:00'),
-(5, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:58:38', '0000-00-00 00:00:00'),
-(6, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:58:56', '0000-00-00 00:00:00'),
-(7, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 14:00:45', '0000-00-00 00:00:00');
+INSERT INTO `cesbie_visitors` (`id`, `staff_id`, `temperature`, `place_of_origin`, `pin_code`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:50:23', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:52:22', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:57:43', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:58:01', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:58:38', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 13:58:56', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, 1, '37.5', 'San Mateo, Rizal', 'zXc12365gfd', '2020-07-22 14:00:45', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, 2, '37.5', 'Marikina City', 'zXc12365gfd', '2020-07-23 07:27:12', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -110,17 +114,18 @@ CREATE TABLE `division` (
   `name` varchar(300) NOT NULL,
   `is_active` int(1) NOT NULL DEFAULT 1,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `division`
 --
 
-INSERT INTO `division` (`id`, `name`, `is_active`, `created_at`, `updated_at`) VALUES
-(1, 'Dost', 0, '2020-07-20 15:01:44', '0000-00-00 00:00:00'),
-(2, 'DFA', 1, '2020-07-20 15:04:54', '0000-00-00 00:00:00'),
-(3, 'DepEd', 1, '2020-07-20 15:43:36', '0000-00-00 00:00:00');
+INSERT INTO `division` (`id`, `name`, `is_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Dost', 0, '2020-07-20 15:01:44', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'DFA', 1, '2020-07-20 15:04:54', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'DepEd', 1, '2020-07-20 15:43:36', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -134,7 +139,8 @@ CREATE TABLE `feedbacks` (
   `overall_experience` int(1) NOT NULL COMMENT '3 = good, 2 = okay, 1 = bad',
   `feedback` longtext NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -148,7 +154,8 @@ CREATE TABLE `services` (
   `name` varchar(300) NOT NULL,
   `is_active` int(1) NOT NULL DEFAULT 1,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -162,15 +169,17 @@ CREATE TABLE `staffs` (
   `fullname` varchar(300) NOT NULL,
   `division_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staffs`
 --
 
-INSERT INTO `staffs` (`id`, `fullname`, `division_id`, `created_at`, `updated_at`) VALUES
-(1, 'Lorenzo Salamante', 1, '2020-07-21 11:55:58', '0000-00-00 00:00:00');
+INSERT INTO `staffs` (`id`, `fullname`, `division_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Lorenzo Salamante', 1, '2020-07-21 11:55:58', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Diane Ocampo', 1, '2020-07-23 07:25:59', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -195,15 +204,16 @@ CREATE TABLE `visitors` (
   `health_condition` varchar(300) NOT NULL,
   `pin_code` varchar(300) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `deleted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `visitors`
 --
 
-INSERT INTO `visitors` (`id`, `fullname`, `agency`, `attached_agency`, `email_address`, `is_have_ecopy`, `photo`, `division_to_visit`, `purpose`, `person_to_visit`, `temperature`, `place_of_origin`, `mobile_number`, `health_condition`, `pin_code`, `created_at`, `updated_at`) VALUES
-(1, 'Elline Ocampo', 2, 1, 'edocampo@myoptimind.com', 1, '1-1595320977_5400290-polaroid-polaroid-frame-png-polaroid-template-marco-polaroid-polaroid-picture-frame-png-820_685_preview.jpg', 3, 0, 1, '36.5', 'San Mateo, Rizal', '09497912581', 'Normal', 'zXc12365gfd', '2020-07-21 12:42:57', '2020-07-21 12:42:57');
+INSERT INTO `visitors` (`id`, `fullname`, `agency`, `attached_agency`, `email_address`, `is_have_ecopy`, `photo`, `division_to_visit`, `purpose`, `person_to_visit`, `temperature`, `place_of_origin`, `mobile_number`, `health_condition`, `pin_code`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Elline Ocampo', 2, 1, 'edocampo@myoptimind.com', 1, '1-1595320977_5400290-polaroid-polaroid-frame-png-polaroid-template-marco-polaroid-polaroid-picture-frame-png-820_685_preview.jpg', 3, 0, 1, '36.5', 'San Mateo, Rizal', '09497912581', 'Normal', 'zXc12365gfdz', '2020-07-21 12:42:57', '2020-07-23 07:48:38', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -277,7 +287,7 @@ ALTER TABLE `agency`
 -- AUTO_INCREMENT for table `cesbie_visitors`
 --
 ALTER TABLE `cesbie_visitors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `division`
@@ -301,7 +311,7 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT for table `staffs`
 --
 ALTER TABLE `staffs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `visitors`
